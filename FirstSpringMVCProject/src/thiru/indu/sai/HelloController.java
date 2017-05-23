b56d9c59b84eadc0f99cf2792c5e2448f41665d7
ThiruvenkadamThangaraj <thiruvenkadamthangaraj@gmail.com>
2017-05-23 17:30:55 -0400

    Add files via upload

---
+++ FirstSpringMVCProject/src/thiru/indu/sai/HelloController.java
@@ -0,0 +1,15 @@
+package thiru.indu.sai;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.web.portlet.ModelAndView;
+
+public class HelloController extends AbstractContller{
+
+	@Override
+	protected ModelAndView handleRequestInternal(HttpServletRequest request,HttpServletResponse response){
+		
+	}
+	
+}
